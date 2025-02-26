932b303ecb41f8563f379edf5728c9ca45e1407b
Cortez <arthurcortez2@gmail.com>
2025-02-26 17:41:29 -0300

    feat: added tailwind lib and configurations, fix: app routing path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app-routing';
 import { AppComponent } from './app.component';
 
+const config: ExtraOptions = {
+  preloadingStrategy: PreloadAllModules,
+  scrollPositionRestoration: 'enabled',
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes, config)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
